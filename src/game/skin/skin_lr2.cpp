a8a2557672447cffb8793591f12ed0cbcac66995
chown2 <kuso@cock.email>
2024-12-28 00:34:21 +0000

    refactor(game): use assert_failed

--- src/game/skin/skin_lr2.cpp
+++ src/game/skin/skin_lr2.cpp
@@ -483,7 +483,7 @@ void SkinLR2::setGaugeDisplayType(unsigned slot, GaugeDisplayType type)
         {
         case PLAYER_SLOT_PLAYER: return GLOBAL_SPRITE_IDX_1PGAUGE;
         case PLAYER_SLOT_TARGET: return GLOBAL_SPRITE_IDX_2PGAUGE;
-        default: LVF_DEBUG_ASSERT(false); return {};
+        default: lunaticvibes::assert_failed("spriteIdxForSlot");
         }
     };
     auto gaugeDisplayTypeToSpriteType = [](GaugeDisplayType type) -> SpriteGaugeGrid::GaugeType {
