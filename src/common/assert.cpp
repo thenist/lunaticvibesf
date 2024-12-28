55e2140d647520aeff021d814311c94d797b785e
chown2 <kuso@cock.email>
2024-12-28 00:34:24 +0000

    refactor(common): remove unused arguments

--- src/common/assert.cpp
+++ src/common/assert.cpp
@@ -4,7 +4,7 @@
 #include <common/log.h>
 #include <common/sysutil.h>
 
-[[noreturn]] static void die_on_assertion_fail(const char* file, int line, const char* msg)
+[[noreturn]] static void die_on_assertion_fail(const char* msg)
 {
     panic("Internal error: assertion failed", msg);
 }
@@ -12,12 +12,12 @@
 void lunaticvibes::assert_failed(const char* msg, std::source_location loc)
 {
     LOG_FATAL << "Fatal assertion failed at " << loc.file_name() << ":" << loc.line() << ": " << msg;
-    die_on_assertion_fail(loc.file_name(), loc.line(), msg);
+    die_on_assertion_fail(msg);
 }
 
 void lunaticvibes::verify_failed(const char* msg, std::source_location loc)
 {
     LOG_ERROR << "Assertion failed at " << loc.file_name() << ":" << loc.line() << ": " << msg;
     if (lunaticvibes::in_test_mode())
-        die_on_assertion_fail(loc.file_name(), loc.line(), msg);
+        die_on_assertion_fail(msg);
 }
