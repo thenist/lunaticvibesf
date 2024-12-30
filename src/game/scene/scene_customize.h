thenist/lunaticvibesf

commit 6a78a65c854c04092626c731cfe2025488f60bc5

fix(game): scene_customize: run update on main loop
There is no synchronization currently, so skin loading basically shows
incomplete state.

src/game/scene/scene_customize.h
@@ -42,7 +42,9 @@ protected:
 
 protected:
     // Looper callbacks
-    void _updateAsync() override;
+    void _updateAsync() override {};
+
+    void update() override;
     void updateStart();
     void updateMain();
     void updateFadeout();

src/game/scene/scene_customize.cpp
@@ -185,8 +185,9 @@ SceneCustomize::~SceneCustomize()
     loopEnd();
 }
 
-void SceneCustomize::_updateAsync()
+void SceneCustomize::update()
 {
+    SceneBase::update();
     if (!gInCustomize && gNextScene != SceneType::SELECT)
         return;
 

commit 94c04a5a3495758e407077075cc4e512d5c39ba4

fix(game): run update loop of virtual customize
Fixes changing options from select not doing anything.

Fixes: 6a78a65c854c ("fix(game): scene_customize: run update on main loop")

src/game/scene/scene_customize.h
@@ -21,6 +21,8 @@ public:
     explicit SceneCustomize(const std::shared_ptr<SkinMgr>& skinMgr);
     ~SceneCustomize() override;
 
+    void update() override;
+
     void setIsVirtual(bool is_virtual) { _is_virtual = is_virtual; };
 
 protected:
@@ -44,7 +46,6 @@ protected:
     // Looper callbacks
     void _updateAsync() override {};
 
-    void update() override;
     void updateStart();
     void updateMain();
     void updateFadeout();

src/game/scene/scene_select.cpp
@@ -1249,7 +1249,8 @@ void SceneSelect::updateFadeout()
 void SceneSelect::update()
 {
     SceneBase::update();
-
+    if (_virtualSceneCustomize)
+        _virtualSceneCustomize->update();
     if (_virtualSceneLoadSongs)
         _virtualSceneLoadSongs->update();
 }