5d66da23dcfe910a449ef5bc9d7829be74cb3db1
chown2 <kuso@cock.email>
2024-12-30 00:49:10 +0000

    refactor(game): disable skin debug menus by default
    They are only rarely needed. It takes just a few seconds to turn them
    on.

--- src/game/skin/skin_lr2_debug.h
+++ src/game/skin/skin_lr2_debug.h
@@ -2,13 +2,8 @@
 
 namespace lunaticvibes
 {
-#ifdef NDEBUG
 inline bool g_enable_imgui_debug_monitor = false;
 inline bool g_enable_show_clicked_sprite = false;
-#else
-inline bool g_enable_imgui_debug_monitor = true;
-inline bool g_enable_show_clicked_sprite = true;
-#endif // NDEBUG
 } // namespace lunaticvibes
 
 inline bool imguiShowMonitorLR2DST = false;
