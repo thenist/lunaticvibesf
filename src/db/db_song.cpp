thenist/lunaticvibesf

commit a76b2dfb3e3041daf1bac1b3125e75ce2f23f8c1

refactor(db): use RAII for transactions

src/db/db_song.cpp
@@ -583,18 +583,13 @@ int SongDB::initializeFolders(const std::vector<Path>& paths)
     std::chrono::system_clock::time_point sessionTimestamp = std::chrono::system_clock::now();
 
     std::atomic<bool> inAddFolderSession = true;
-    transactionStart();
     sessionFuture = std::async(std::launch::async, [&]() {
         SetThreadName("SongFolderInit");
         while (inAddFolderSession)
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             if (inAddFolderSession && std::chrono::system_clock::now() - sessionTimestamp >= std::chrono::seconds(10))
-            {
                 sessionTimestamp = std::chrono::system_clock::now();
-                commit();
-                transactionStart();
-            }
         }
     });
 
@@ -607,7 +602,6 @@ int SongDB::initializeFolders(const std::vector<Path>& paths)
     }
 
     inAddFolderSession = false;
-    commit();
     sessionFuture.wait_for(std::chrono::seconds(10));
 
     waitLoadingFinish();

src/db/db_conn.cpp
@@ -243,69 +243,47 @@ int SQLite::exec(const std::string_view zsql, std::initializer_list<std::any> ar
     return SQLITE_OK;
 }
 
-void SQLite::transactionStart()
+SQLite::Transaction::Transaction(SQLite& db) : _db(db)
 {
-    if (inTransaction)
-    {
-        LOG_ERROR << "[sqlite3] [" << tag << "] Ignoring nested transactionStart";
-        LVF_DEBUG_ASSERT(false && "nested transactionStart");
-        return;
-    }
-    inTransaction = true;
+    LVF_ASSERT(!db._isinTransaction && "Transaction()");
+    db._isinTransaction = true;
+    db.exec("BEGIN");
+}
 
+SQLite::Transaction::~Transaction()
+{
+    if (!_finished)
+        rollback();
+    _db._isinTransaction = false;
+}
+
+void SQLite::Transaction::commit()
+{
+    LVF_ASSERT(!_finished && "commit");
+    exec("COMMIT");
+    _finished = true;
+}
+
+void SQLite::Transaction::rollback()
+{
+    LVF_ASSERT(!_finished && "rollback");
+    exec("ROLLBACK");
+    _finished = true;
+}
+
+void SQLite::Transaction::exec(const std::string_view sql)
+{
     sqlite3_stmt* stmt = nullptr;
-    int ret = sqlite3_prepare_v3(_db.get(), "BEGIN", 6, 0, &stmt, nullptr);
-    if (ret)
-    {
-        LOG_ERROR << "[sqlite3] " << tag << ": " << "sqlite3_prepare_v3 error";
-        return;
-    }
+    int ret = sqlite3_prepare_v3(_db._db.get(), sql.data(), static_cast<int>(sql.size()), 0, &stmt, nullptr);
+    LVF_ASSERT(ret == SQLITE_OK && "sqlite3_prepare_v3");
     ret = sqlite3_step(stmt);
     if (ret != SQLITE_OK && ret != SQLITE_ROW && ret != SQLITE_DONE)
     {
-        LOG_ERROR << "[sqlite3] " << tag << ": " << "Transaction start failed with error: " << errmsg();
+        LOG_ERROR << "[sqlite3] " << _db.tag << ": Transaction '" << sql << "' failed: " << _db.errmsg();
     }
     else
     {
-        LOG_DEBUG << "[sqlite3] " << tag << ": " << "Transaction start";
-    }
-    sqlite3_finalize(stmt);
-}
-
-void SQLite::commit()
-{
-    commitOrRollback("COMMIT");
-}
-void SQLite::rollback()
-{
-    commitOrRollback("ROLLBACK");
-}
-void SQLite::commitOrRollback(const std::string_view sql)
-{
-    if (!inTransaction)
-    {
-        LOG_ERROR << "[sqlite3] [" << tag << "] commit or rollback outside of a transaction";
-        LVF_DEBUG_ASSERT(false && "commit or rollback outside of a transaction");
-        return;
-    }
-
-    inTransaction = false;
-
-    sqlite3_stmt* stmt = nullptr;
-    int ret = sqlite3_prepare_v3(_db.get(), sql.data(), static_cast<int>(sql.size()), 0, &stmt, nullptr);
-    if (ret)
-    {
-        LOG_ERROR << "[sqlite3] " << tag << ": " << "sqlite3_prepare_v3 error";
-        return;
-    }
-    ret = sqlite3_step(stmt);
-    if (ret != SQLITE_OK && ret != SQLITE_ROW && ret != SQLITE_DONE)
-    {
-        LOG_ERROR << "[sqlite3] " << tag << ": " << "Transaction '" << sql << "' failed: " << errmsg();
-    }
-    else
-    {
-        LOG_DEBUG << "[sqlite3] " << tag << ": " << "Transaction '" << sql << "' success";
+        LOG_DEBUG << "[sqlite3] " << _db.tag << ": Transaction '" << sql << "' success";
     }
     sqlite3_finalize(stmt);
 }
@@ -329,33 +307,26 @@ bool SQLite::applyMigration(std::string_view name, const std::function<bool()>& 
         return false;
     }
 
-    transactionStart();
-
     auto didAlreadyMigrate = query("SELECT EXISTS (SELECT 1 FROM __lvf_schema_migrations WHERE name=?);", {name});
     LVF_DEBUG_ASSERT(didAlreadyMigrate.size() == 1 && didAlreadyMigrate[0].size() == 1);
     if (ANY_INT(didAlreadyMigrate[0][0]) == 1)
     {
         LOG_VERBOSE << "[sqlite3] Migration has already been applied";
-        commit();
         return true;
     }
+
+    Transaction transaction(*this);
+
     if (!migrate())
     {
         LOG_ERROR << "[sqlite3] Migration '" << name << "' failed";
-        rollback();
         return false;
     }
 
     ret = exec("INSERT INTO __lvf_schema_migrations(name, date) VALUES (?, datetime());", {name});
-    if (ret != SQLITE_OK)
-    {
-        LOG_FATAL << "[sqlite3] Insertion into __lvf_schema_migrations failed: " << errmsg();
-        LVF_DEBUG_ASSERT(false && "Insertion into __lvf_schema_migrations failed");
-        rollback();
-        return false;
-    }
+    LVF_ASSERT(ret == SQLITE_OK && "INSERT INTO __lvf_schema_migrations");
 
-    commit();
+    transaction.commit();
     LOG_VERBOSE << "[sqlite3] Migration has been successfully applied";
     return true;
 }

src/db/db_conn.h
@@ -54,7 +54,7 @@ class SQLite
 private:
     mutable lunaticvibes::SqlitePtr _db;
     std::string tag;
-    bool inTransaction = false;
+    bool _isinTransaction = false;
 
 public:
     enum class OpenMode
@@ -85,14 +85,26 @@ protected:
     [[nodiscard]] bool applyMigration(std::string_view name, const std::function<bool()>& migrate);
     [[nodiscard]] bool isReadOnly() const;
 
-private:
-    void commitOrRollback(const std::string_view sql);
-
 public:
-    // TODO: RAII.
-    void transactionStart();
-    void commit();
-    void rollback();
+    class Transaction
+    {
+    public:
+        Transaction(SQLite&);
+        ~Transaction();
+
+        void commit();
+        void rollback();
+
+        Transaction(const Transaction&) = delete;
+        Transaction(Transaction&&) = delete;
+        Transaction& operator=(const Transaction&) = delete;
+        Transaction& operator=(Transaction&&) = delete;
+
+    private:
+        void exec(std::string_view sql);
+        SQLite& _db;
+        bool _finished = false;
+    };
 
     void optimize();
     [[nodiscard]] const char* errmsg() const;

src/db/db_score.cpp
@@ -430,7 +430,7 @@ bool ScoreDB::isBmsPbCacheEmpty()
 void ScoreDB::rebuildBmsPbCache()
 {
     LOG_DEBUG << "[ScoreDB] Asked to rebuild BMS score PB cache";
-    transactionStart();
+    Transaction transaction(*this);
     exec("DELETE FROM score_cache_bms");
     for (const auto& raw_score : query("SELECT * FROM score_bms ORDER BY addtime"))
     {
@@ -450,7 +450,7 @@ void ScoreDB::rebuildBmsPbCache()
         convertHistoryScoreBms(*score, raw_score);
         updateCachedChartPbBms(HashMD5{md5}, *score);
     }
-    commit();
+    transaction.commit();
     preloadScore();
 }
 

test/db/test_db_conn.cpp
@@ -4,6 +4,44 @@
 
 static constexpr auto&& IN_MEMORY_DB_PATH = ":memory:";
 
+TEST(DbConn, CommitRollbackWorks)
+{
+    struct TestSQLite : public SQLite
+    {
+        TestSQLite() : SQLite(IN_MEMORY_DB_PATH, "CommitRollbackWorks") {};
+        void runTest()
+        {
+            ASSERT_EQ(exec("CREATE TABLE some_data(id INTEGER);"), SQLITE_OK);
+            auto insert = [this]() { ASSERT_EQ(exec("INSERT INTO some_data(id) VALUES (1337);"), SQLITE_OK); };
+            auto check_fetch = [this]() {
+                auto res = query("SELECT id FROM some_data;");
+                return res.size() == 1 && res[0].size() == 1 && ANY_INT(res[0][0]) == 1337;
+            };
+
+            {
+                Transaction transaction(*this);
+                insert();
+                transaction.rollback();
+            }
+            EXPECT_FALSE(check_fetch());
+
+            {
+                Transaction transaction(*this);
+                insert();
+            }
+            EXPECT_FALSE(check_fetch());
+
+            {
+                Transaction transaction(*this);
+                insert();
+                transaction.commit();
+            }
+            EXPECT_TRUE(check_fetch());
+        }
+    };
+    TestSQLite{}.runTest();
+}
+
 TEST(DbConn, MigrationSystemWorks)
 {
     struct TestSQLite : public SQLite

commit 3fe414995fa520627f5025cfe95d172613980f82

fix(db): use transactions for adding songs to DB
Fixes massive performance regression on song importing.
This surprisingly doesn't show up in the profiler at all.

Fixes: a76b2dfb3e30 ("refactor(db): use RAII for transactions")

src/db/db_song.cpp
@@ -579,17 +579,16 @@ int SongDB::initializeFolders(const std::vector<Path>& paths)
 {
     resetAddSummary();
 
-    std::future<void> sessionFuture;
-    std::chrono::system_clock::time_point sessionTimestamp = std::chrono::system_clock::now();
-
     std::atomic<bool> inAddFolderSession = true;
-    sessionFuture = std::async(std::launch::async, [&]() {
-        SetThreadName("SongFolderInit");
+    std::future<void> flush_db_task = std::async(std::launch::async, [&inAddFolderSession, this]() {
+        SetThreadName("flush db task");
         while (inAddFolderSession)
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            if (inAddFolderSession && std::chrono::system_clock::now() - sessionTimestamp >= std::chrono::seconds(10))
-                sessionTimestamp = std::chrono::system_clock::now();
+            Transaction t(*this);
+            // PERF: without transactions, inserts and updates are immediately flushed to disk, making the whole process
+            // UNGODLY slow.
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            t.commit();
         }
     });
 
@@ -602,7 +601,6 @@ int SongDB::initializeFolders(const std::vector<Path>& paths)
     }
 
     inAddFolderSession = false;
-    sessionFuture.wait_for(std::chrono::seconds(10));
 
     waitLoadingFinish();
 