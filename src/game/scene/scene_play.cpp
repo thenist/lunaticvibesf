thenist/lunaticvibesf

commit e1c2b61c3d6723049b8369d9bd147ee57f3917b8

fix: allow absolute LR2 and BMS chart paths
This serves no security benefit, as you can still use `../../`.
Allows absolute path in `LR2Path` config setting.

src/game/scene/scene_play.cpp
@@ -1208,18 +1208,12 @@ void ScenePlay::loadChart()
                     if (shouldDiscard(*this))
                         return;
                     Path pWav = PathFromUTF8(wav);
-                    if (pWav.is_absolute())
-                    {
-                        LOG_WARNING << "[Play] Absolute path to sample, this is forbidden";
-                    }
-                    else
-                    {
-                        fs::path p{chartDir / pWav};
+                    LOG_WARNING << "[Play] Absolute path to sample, this is forbidden";
+                    fs::path p{chartDir / pWav};
 #ifndef _WIN32
-                        p = lunaticvibes::resolve_windows_path(lunaticvibes::u8str(p));
+                    p = lunaticvibes::resolve_windows_path(lunaticvibes::u8str(p));
 #endif // _WIN32
-                        SoundMgr::loadNoteSample(p, i);
-                    }
+                    SoundMgr::loadNoteSample(p, i);
                     ++gPlayContext.wavLoaded;
                 });
             }
@@ -1280,18 +1274,11 @@ void ScenePlay::loadChart()
                         if (shouldDiscard(*this))
                             break;
                         const auto pBmp = PathFromUTF8(bmp);
-                        if (pBmp.is_absolute())
-                        {
-                            LOG_WARNING << "[Play] Absolute path to BGA picture, this is forbidden";
-                        }
-                        else
-                        {
-                            fs::path p{chartDir / pBmp};
+                        fs::path p{chartDir / pBmp};
 #ifndef _WIN32
-                            p = lunaticvibes::resolve_windows_path(lunaticvibes::u8str(p));
+                        p = lunaticvibes::resolve_windows_path(lunaticvibes::u8str(p));
 #endif // _WIN32
-                            gPlayContext.bgaTexture->addBmp(i, p);
-                        }
+                        gPlayContext.bgaTexture->addBmp(i, p);
                         ++gPlayContext.bmpLoaded;
                     }
                 }

src/common/utils.cpp
@@ -420,10 +420,7 @@ Path convertLR2Path(const std::string& lr2path, std::string_view relative_path_u
     if (raw.empty())
         return {};
     if (auto p = PathFromUTF8(raw); p.is_absolute())
-    {
-        LOG_WARNING << "[Utils] Absolute LR2 paths are forbidden: " << raw;
-        return {};
-    }
+        return p;
 
     std::string_view prefix = raw.substr(0, 2);
     if (prefix == "./" || prefix == ".\\")

src/game/scene/scene_select.cpp
@@ -3173,17 +3173,10 @@ bool tryLoadDedicatedPreview(ChartFormatBMS& bms)
     if (!bms.dedicatedPreview.empty())
     {
         Path pWav = PathFromUTF8(bms.dedicatedPreview);
-        if (pWav.is_absolute())
-        {
-            LOG_WARNING << "[Select] #PREVIEW contains absolute path, this is forbidden";
-        }
-        else
-        {
-            pWav = bms.getDirectory() / pWav;
-            if (SoundMgr::loadNoteSample(pWav, STANDALONE_PREVIEW_SAMPLE_INDEX) == 0)
-                return true;
-            LOG_WARNING << "[Select] Failed to load #PREVIEW sample (" << pWav << ")";
-        }
+        pWav = bms.getDirectory() / pWav;
+        if (SoundMgr::loadNoteSample(pWav, STANDALONE_PREVIEW_SAMPLE_INDEX) == 0)
+            return true;
+        LOG_WARNING << "[Select] Failed to load #PREVIEW sample (" << pWav << ")";
     }
 
     // check if preview(*).ogg is valid
@@ -3339,11 +3332,6 @@ void SceneSelect::updatePreview()
                             if (shouldDiscard(*this, bms))
                                 return;
                             Path pWav = PathFromUTF8(wav);
-                            if (pWav.is_absolute())
-                            {
-                                LOG_WARNING << "[Select] Absolute path to sample, this is forbidden";
-                                return;
-                            }
                             fs::path p{chartDir / pWav};
 #ifndef _WIN32
                             p = lunaticvibes::resolve_windows_path(lunaticvibes::u8str(p));

test/common/test_path.cpp
@@ -10,11 +10,12 @@ TEST(Paths, ResolvesLr2PathsCorrectlty)
 
 // fs::path::is_absolute only works for native paths, tested on libstdc++ with GCC 13.2.1 and some MSVC version.
 #ifdef _WIN32
-    EXPECT_EQ(conv("D:\\Games\\LunaticVibes", "C:\\something\\somewhere"), "");
-    EXPECT_EQ(conv("D:\\Games\\LunaticVibes", R"(D:\Games\LunaticVibes\LR2files\Config\config.xml)"), "");
+    EXPECT_EQ(conv("D:\\Games\\LunaticVibes", "C:\\something\\somewhere"), "C:\\something\\somewhere");
+    EXPECT_EQ(conv("D:\\Games\\LunaticVibes", R"(D:\Games\LunaticVibes\LR2files\Config\config.xml)"),
+              R"(D:\Games\LunaticVibes\LR2files\Config\config.xml)");
 #else
-    EXPECT_EQ(conv("/home/me/lv", "/etc/passwd"), "");
-    EXPECT_EQ(conv("/opt/lv", "/opt/lv/LR2files/Config/config.xml"), "");
+    EXPECT_EQ(conv("/home/me/lv", "/etc/passwd"), "/etc/passwd");
+    EXPECT_EQ(conv("/opt/lv", "/opt/lv/LR2files/Config/config.xml"), "/opt/lv/LR2files/Config/config.xml");
 #endif // _WIN32
 
     EXPECT_EQ(conv("/home/me/lv", u8" テスト "), " テスト ");

commit 1d699fb9980137705c43f7324881d995f05f6c5a

fix(game): remove wrong log message
Fixes: e1c2b61c3d67 ("fix: allow absolute LR2 and BMS chart paths")

src/game/scene/scene_play.cpp
@@ -1212,7 +1212,6 @@ void ScenePlay::loadChart()
                     if (shouldDiscard(*this))
                         return;
                     Path pWav = PathFromUTF8(wav);
-                    LOG_WARNING << "[Play] Absolute path to sample, this is forbidden";
                     fs::path p{chartDir / pWav};
 #ifndef _WIN32
                     p = lunaticvibes::resolve_windows_path(lunaticvibes::u8str(p));