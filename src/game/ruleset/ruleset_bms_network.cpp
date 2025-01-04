thenist/lunaticvibesf

commit 19039cb3195dacd4b5d011162a9f65ebb705c03f

refactor(game): deduplicate result lamp calculating in arena
Didn't test. Looks fine to eye.

src/game/ruleset/ruleset_bms_network.cpp
@@ -102,28 +102,9 @@ void RulesetBMSNetwork::update(const lunaticvibes::Time& t)
         State::set(N(IndexNumber::ARENA_PLAYDATA_PLAYER_EX_DIFF), player->getExScore() - exScore);
 
     Option::e_lamp_type lamp = Option::LAMP_NOPLAY;
-    // TODO: deduplicate with calculateLamp. Seems to be the same but isFinished().
     if (!isNoScore() && isFinished())
-    {
-        if (_basic.judge[JUDGE_CB] == 0)
-        {
-            if (_basic.acc >= 100.0)
-                lamp = Option::LAMP_MAX;
-            else if (_basic.judge[JUDGE_GOOD] == 0)
-                lamp = Option::LAMP_PERFECT;
-            else if (isCleared())
-                lamp = Option::LAMP_FULLCOMBO;
-        }
-        else if (isFailed())
-        {
-            lamp = Option::LAMP_FAILED;
-        }
-        else
-        {
-            lamp = gaugeToOption(getGaugeType());
-        }
-    }
-    State::set(O(IndexOption::ARENA_PLAYDATA_CLEAR_TYPE), std::min(lamp, saveLampMax));
+        lamp = calculateLamp();
+    State::set(O(IndexOption::ARENA_PLAYDATA_CLEAR_TYPE), lamp);
 }
 
 std::vector<unsigned char> RulesetBMSNetwork::packInit(const std::shared_ptr<RulesetBMS>& local)

src/game/ruleset/ruleset_bms.cpp
@@ -1950,16 +1950,16 @@ Option::e_lamp_type RulesetBMS::calculateLamp() const
             lamp = Option::LAMP_MAX;
         else if (_basic.judge[JUDGE_GOOD] == 0)
             lamp = Option::LAMP_PERFECT;
-        else
+        else if (isCleared())
             lamp = Option::LAMP_FULLCOMBO;
     }
-    else if (!isFailed())
+    else if (isFailed())
     {
-        lamp = gaugeToOption(getGaugeType());
+        lamp = Option::LAMP_FAILED;
     }
     else
     {
-        lamp = Option::LAMP_FAILED;
+        lamp = gaugeToOption(getGaugeType());
     }
     return std::min(lamp, saveLampMax);
 }

commit 2ee4f733f57615499e44b05b95d039d44184fc9a

refactor(game): remove unused function
Fixes: 19039cb3195d ("refactor(game): deduplicate result lamp calculating in arena")

src/game/ruleset/ruleset_bms_network.cpp
@@ -16,25 +16,6 @@ RulesetBMSNetwork::RulesetBMSNetwork(GameModeKeys keys, unsigned playerIndex)
     _judgeScratch = false;
 }
 
-static Option::e_lamp_type gaugeToOption(RulesetBMS::GaugeType _gauge)
-{
-    using GT = RulesetBMS::GaugeType;
-    switch (_gauge)
-    {
-    case GT::HARD: return Option::LAMP_HARD;
-    case GT::EXHARD: return Option::LAMP_EXHARD;
-    case GT::DEATH: return Option::LAMP_FULLCOMBO;
-    case GT::P_ATK: // return Option::LAMP_FULLCOMBO; // TODO: check this.
-    case GT::G_ATK: // return Option::LAMP_FULLCOMBO; // TODO: check this.
-    case GT::GROOVE: return Option::LAMP_NORMAL;
-    case GT::EASY: return Option::LAMP_EASY;
-    case GT::ASSIST: return Option::LAMP_ASSIST;
-    case GT::GRADE:
-    case GT::EXGRADE: return Option::LAMP_NOPLAY;
-    }
-    lunaticvibes::assert_failed("gaugeToOption");
-}
-
 void RulesetBMSNetwork::update(const lunaticvibes::Time& t)
 {
     if (!_hasStartTime)