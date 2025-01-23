thenist/lunaticvibesf

commit 09dd31a1fbc44f0ace4874c4a841fa74e9c7872e

refactor(game): assert_failed getChart

src/game/scene/scene_select.cpp
@@ -1983,18 +1983,13 @@ void SceneSelect::inputGamePressReadme(InputMask& input, const lunaticvibes::Tim
     }
 }
 
-std::shared_ptr<ChartFormatBase> getChart(EntryBase& entry)
+static std::shared_ptr<ChartFormatBase> getChart(EntryBase& entry)
 {
-
     if (entry.type() == eEntryType::SONG || entry.type() == eEntryType::RIVAL_SONG)
-    {
         return reinterpret_cast<EntryFolderSong&>(entry).getCurrentChart();
-    }
     if (entry.type() == eEntryType::CHART || entry.type() == eEntryType::RIVAL_CHART)
-    {
         return reinterpret_cast<EntryChart&>(entry)._file;
-    }
-    return nullptr;
+    lunaticvibes::assert_failed("getChart");
 };
 
 // NOTE: don't forget to lock mutex for 'entries'.

commit d9d2d974a69b19f4dae2326c03962ed24b0c1755

fix(game): allow getChart with unsupported type
Fixes https://github.com/chown2/lunaticvibesf/issues/187 "Crashes when
selecting a random select without charts with unavailable difficulty".

Fixes: 09dd31a1fbc4 ("refactor(game): assert_failed getChart")

src/game/scene/scene_select.cpp
@@ -1990,7 +1990,9 @@ static std::shared_ptr<ChartFormatBase> getChart(EntryBase& entry)
         return reinterpret_cast<EntryFolderSong&>(entry).getCurrentChart();
     if (entry.type() == eEntryType::CHART || entry.type() == eEntryType::RIVAL_CHART)
         return reinterpret_cast<EntryChart&>(entry)._file;
-    lunaticvibes::assert_failed("getChart");
+    // E.g. for re-roll if we get RANDOM_CHART itself first.
+    LOG_VERBOSE << "[Select] No chart for entry " << static_cast<int>(entry.type());
+    return nullptr;
 };
 
 // NOTE: don't forget to lock mutex for 'entries'.