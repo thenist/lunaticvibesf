671258712659171cbefb420adb116ba89047fa13
chown2 <kuso@cock.email>
2025-01-27 22:29:30 +0000

    Revert "refactor(common): simplify string join with ranges"
    This reverts commit 1b640c8c36535fad9ea3c461db762f2cec9b0c34.
    
    Only supported since GCC 14, but ci uses GCC 13.

--- src/common/str_utils.h
+++ src/common/str_utils.h
@@ -15,7 +15,19 @@ void assign(std::span<char> to, std::string_view from);
 template <class Proj = std::identity>
 [[nodiscard]] inline std::string join(char sep, std::ranges::input_range auto v, Proj proj = std::identity{})
 {
-    return std::views::transform(v, proj) | std::views::join_with(sep) | std::ranges::to<std::string>();
+    using std::begin, std::end;
+    auto it = begin(v);
+    auto e = end(v);
+    if (it == e)
+        return {};
+    std::string s;
+    s += *it++;
+    for (; it != e; ++it)
+    {
+        s += sep;
+        s += proj(*it);
+    }
+    return s;
 }
 
 template <class T> void split(const std::string_view raw, const char delim, std::vector<T>& out)
