69e3c603df4cdd9f94eb83189e9fb35fa5799793
chown2 <kuso@cock.email>
2025-01-27 16:05:23 +0000

    refactor(common): shut up false positive warning

--- src/common/str_utils.cpp
+++ src/common/str_utils.cpp
@@ -10,6 +10,6 @@ void lunaticvibes::assign(std::span<char> to, std::string_view from)
     if (arrn == 0)
         arrn = 1; // Saturating subtraction.
     const size_t bytes = std::min(arrn - 1, from.size());
-    strncpy(to.data(), from.data(), bytes);
+    strncpy(to.data(), from.data(), bytes); // NOLINT(bugprone-suspicious-stringview-data-usage): computed on prev line
     to[bytes] = '\0';
 }
