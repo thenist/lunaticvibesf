1b640c8c36535fad9ea3c461db762f2cec9b0c34
chown2 <kuso@cock.email>
2025-01-27 16:02:57 +0000

    refactor(common): simplify string join with ranges

--- src/common/str_utils.h
+++ src/common/str_utils.h
@@ -14,19 +14,7 @@ void assign(std::span<char> to, std::string_view from);
 template <class Proj = std::identity>
 [[nodiscard]] inline std::string join(char sep, std::ranges::input_range auto v, Proj proj = std::identity{})
 {
-    using std::begin, std::end;
-    auto it = begin(v);
-    auto e = end(v);
-    if (it == e)
-        return {};
-    std::string s;
-    s += *it++;
-    for (; it != e; ++it)
-    {
-        s += sep;
-        s += proj(*it);
-    }
-    return s;
+    return std::views::transform(v, proj) | std::views::join_with(sep) | std::ranges::to<std::string>();
 }
 
 } // namespace lunaticvibes
