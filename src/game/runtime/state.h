thenist/lunaticvibesf

commit 91e7375b316ec736992e63d53fc2b67a906d4fd2

perf(game): avoid some copies with State::set, esp. with std::string

src/game/runtime/state.h
@@ -14,6 +14,8 @@
 #include <shared_mutex>
 #include <string>
 #include <string_view>
+#include <type_traits>
+#include <utility>
 
 // Global state value manager
 class State
@@ -30,7 +32,10 @@ protected:
 		using ValType = Value;
 
 	public:
-		StateContainer() : _data{ Value() }, _dataDefault{ Value() } { static_assert(_size > 0); }
+		StateContainer() : _data{{}}, _dataDefault{{}}
+		{
+			static_assert(_size > 0);
+		}
 		StateContainer(Value defVal) : StateContainer()
 		{
 			_data.fill(defVal);
@@ -50,10 +55,23 @@ protected:
 				std::shared_lock l{_mutex};
 				return _data.data()[idx];
 			}
-			return Value();
+			return {};
 		}
 
-		bool set(Key n, Value value)
+		bool set(Key n, const Value& value)
+		{
+			size_t idx = static_cast<size_t>(n);
+			if (idx < _size)
+			{
+				std::unique_lock l{_mutex};
+				_data[idx] = value;
+				return true;
+			}
+			return false;
+		}
+
+		template <typename = typename std::enable_if<std::is_same_v<Value, std::string>>>
+		bool set(Key n, std::string_view value)
 		{
 			size_t idx = (size_t)n;
 			if (idx < _size)
@@ -70,7 +88,7 @@ protected:
 			size_t idx = (size_t)n;
 			if (idx < _size)
 			{
-				_dataDefault[idx] = value;
+				_dataDefault[idx] = std::move(value);
 				return true;
 			}
 			return false;

src/game/runtime/state.cpp
@@ -76,7 +76,7 @@ bool State::get(IndexSwitch ind)
 
 bool State::set(IndexText ind, std::string_view val)
 {
-	return _inst.gTexts.set(ind, std::string(val));
+	return _inst.gTexts.set(ind, val);
 }
 
 std::string State::get(IndexText ind)

commit 3104cdae3d79716e51bcc92f474afa1316a5e280

refactor(game): remove broken enable_if
Fixes: 91e7375b316e ("perf(game): avoid some copies with State::set, esp. with std::string")

src/game/runtime/state.h
@@ -13,7 +13,6 @@
 #include <shared_mutex>
 #include <string>
 #include <string_view>
-#include <type_traits>
 #include <utility>
 
 // Global state value manager
@@ -66,7 +65,7 @@ protected:
             return false;
         }
 
-        template <typename = typename std::enable_if<std::is_same_v<Value, std::string>>>
+        // PERF: avoid forcing copies for strings.
         bool set(Key n, std::string_view value)
         {
             auto idx = (size_t)n;