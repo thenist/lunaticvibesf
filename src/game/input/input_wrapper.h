thenist/lunaticvibesf

commit 3ba853a0186ba2dc9165cba31156a30be56876a5

fix(game): rework handling of input events in main loop
Quickly drain the event queue into temporary vectors and process without
holding locks.

src/game/input/input_wrapper.h
@@ -7,7 +7,7 @@
 
 #include <array>
 #include <map>
-#include <shared_mutex>
+#include <mutex>
 #include <utility>
 
 // InputWrapper
@@ -21,6 +21,7 @@ public:
     static constexpr unsigned release_delay_ms = 5;
 
 private:
+    // Input queue, separately shared
     struct InputPressEvent
     {
         lunaticvibes::InputMaskTimes times;
@@ -43,35 +44,42 @@ private:
         double delta1;
         double delta2;
     };
+    std::mutex _inputQueueMutex;
     std::queue<InputHoldEvent> _input_hold_events;
     std::queue<InputPressEvent> _input_press_events;
     std::queue<InputReleaseEvent> _input_release_events;
     std::queue<ScratchEvent> _scratch_events;
+    // These will be drained from the queue to be processed without holding the mutex.
+    std::vector<InputHoldEvent> _drained_input_hold_events;
+    std::vector<InputPressEvent> _drained_input_press_events;
+    std::vector<InputReleaseEvent> _drained_input_release_events;
+    std::vector<ScratchEvent> _drained_scratch_events;
 
-    mutable std::shared_mutex _inputMutex;
+    // Const data
+    bool _background;
+
+    // Async thread data
     AsyncLooper _looper;
     lunaticvibes::Time _prevUpdateEnd;
-
-protected:
     std::array<std::pair<long long, bool>, Input::KEY_COUNT> _inputBuffer{{{0, false}}};
     std::array<long long, Input::KEY_COUNT> _releaseBuffer{-1};
-    int _cursor_x = 0, _cursor_y = 0;
-    bool _background = false;
-
     InputMask _prev = 0;
     InputMask _curr = 0;
-
-    bool scratchAxisSet = false;
     double scratchAxisPrev[2] = {0.};
     double scratchAxisCurr[2] = {0.};
+    bool scratchAxisSet = false;
 
+    // Shared data
+    mutable std::mutex _inputMutex;
+    InputMask _holding = 0;
+    int _cursor_x = 0;
+    int _cursor_y = 0;
     bool mergeInput = false;
 
 public:
     InputWrapper(unsigned rate = 1000, bool background = false);
     ~InputWrapper();
 
-public:
     void loopEnd() { _looper.loopEnd(); }
     void loopStart() { _looper.loopStart(); }
 
@@ -90,15 +98,13 @@ public:
 
     [[nodiscard]] InputMask Holding() const
     {
-        // FIXME: this will probably recursively lock if this function is called from a callback in processInput.
-        // std::shared_lock l(_inputMutex);
-        return _prev & _curr;
+        std::lock_guard l(_inputMutex);
+        return _holding;
     }
 
     [[nodiscard]] std::pair<int, int> getCursorPos() const
     {
-        // FIXME: this will probably recursively lock if this function is called from a callback in processInput.
-        // std::shared_lock l(_inputMutex);
+        std::lock_guard l(_inputMutex);
         return {_cursor_x, _cursor_y};
     }
 

src/game/input/input_wrapper.cpp
@@ -12,7 +12,7 @@
 #include <game/runtime/generic_info.h>
 
 InputWrapper::InputWrapper(unsigned rate, bool background)
-    : _looper{"InputLoop", std::bind_front(&InputWrapper::loopAsync, this), rate}, _background(background)
+    : _background(background), _looper{"InputLoop", std::bind_front(&InputWrapper::loopAsync, this), rate}
 {
 }
 
@@ -84,7 +84,7 @@ void InputWrapper::loopAsync()
         double d2 = normalizeLinearGrowth(scratchAxisPrev[1], scratchAxisCurr[1]) * InputMgr::getDeadzone(Input::S2A);
         if (d1 != 0. || d2 != 0.)
         {
-            std::unique_lock l(_inputMutex);
+            std::unique_lock l(_inputQueueMutex);
             _scratch_events.emplace(now, d1, d2);
         }
     }
@@ -232,17 +232,17 @@ void InputWrapper::loopAsync()
 
     if (p.any())
     {
-        std::unique_lock l(_inputMutex);
+        std::unique_lock l(_inputQueueMutex);
         _input_press_events.emplace(pTimes, now, p);
     }
     if (h.any())
     {
-        std::unique_lock l(_inputMutex);
+        std::unique_lock l(_inputQueueMutex);
         _input_hold_events.emplace(now, h);
     }
     if (r.any())
     {
-        std::unique_lock l(_inputMutex);
+        std::unique_lock l(_inputQueueMutex);
         _input_release_events.emplace(now, r);
     }
 
@@ -254,38 +254,41 @@ void InputWrapper::loopAsync()
 
 void InputWrapper::processInput()
 {
-    // FIXME: C++ Core Guidelines CP.22
-    std::unique_lock l(_inputMutex);
-
-    while (!_input_press_events.empty())
     {
-        auto& event = _input_press_events.front();
+        std::unique_lock l(_inputQueueMutex);
+        auto drain = []<typename T>(std::queue<T>& q, std::vector<T>& v) {
+            while (!q.empty())
+            {
+                v.push_back(std::move(q.front()));
+                q.pop();
+            }
+        };
+        drain(_input_press_events, _drained_input_press_events);
+        drain(_input_hold_events, _drained_input_hold_events);
+        drain(_input_release_events, _drained_input_release_events);
+        drain(_scratch_events, _drained_scratch_events);
+    }
+
+    for (auto& event : _drained_input_press_events)
+    {
         for (auto& [cbname, callback] : _pCallbackMap)
             callback(event.mask, event.t);
         for (auto& [cbname, callback] : _pCallbackMapNew)
             callback(event.mask, event.t, event.times);
-        _input_press_events.pop();
     }
+    _drained_input_press_events.clear();
 
-    while (!_input_hold_events.empty())
-    {
-        auto& event = _input_hold_events.front();
+    for (auto& event : _drained_input_hold_events)
         for (auto& [cbname, callback] : _hCallbackMap)
             callback(event.mask, event.t);
-        _input_hold_events.pop();
-    }
+    _drained_input_hold_events.clear();
 
-    while (!_input_release_events.empty())
-    {
-        auto& event = _input_release_events.front();
+    for (auto& event : _drained_input_release_events)
         for (auto& [cbname, callback] : _rCallbackMap)
             callback(event.mask, event.t);
-        _input_release_events.pop();
-    }
+    _drained_input_release_events.clear();
 
-    while (!_scratch_events.empty())
-    {
-        auto& event = _scratch_events.front();
+    for (auto& event : _drained_scratch_events)
         for (auto& [cbname, callback] : _aCallbackMap)
         {
             if (mergeInput)
@@ -293,8 +296,7 @@ void InputWrapper::processInput()
             else
                 callback(event.delta1, event.delta2, event.t);
         }
-        _scratch_events.pop();
-    }
+    _drained_scratch_events.clear();
 }
 
 double InputWrapper::getJoystickAxis(size_t device, Input::Joystick::Type type, size_t index)

commit 94bc948f5f0c0f1a847f66c7390ab8c3d9b55a6b

fix(game): make input wrapper actually return keys we are holding
Fixes: 3ba853a0186b ("fix(game): rework handling of input events in main loop")

src/game/input/input_wrapper.h
@@ -71,7 +71,6 @@ private:
 
     // Shared data
     mutable std::mutex _inputMutex;
-    InputMask _holding = 0;
     int _cursor_x = 0;
     int _cursor_y = 0;
     bool mergeInput = false;
@@ -99,7 +98,7 @@ public:
     [[nodiscard]] InputMask Holding() const
     {
         std::lock_guard l(_inputMutex);
-        return _holding;
+        return _prev & _curr;
     }
 
     [[nodiscard]] std::pair<int, int> getCursorPos() const