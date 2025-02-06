thenist/lunaticvibesf

commit 0c3501275562dd661ad9fada61911f418dc96bdc

refactor(game): explicitly include SDL_main for WinMain

src/game/game.cpp
@@ -41,6 +41,10 @@
 #include <timeapi.h>
 #endif // WIN32
 
+#ifdef RENDER_SDL2
+#include <SDL_main.h> // WinMain implementation
+#endif // RENDER_SDL2
+
 #include <boost/nowide/args.hpp>
 
 #include <curl/curl.h>

src/game/CMakeLists.txt
@@ -150,7 +150,7 @@ target_precompile_headers(gamelib REUSE_FROM common)
 
 #############################################################
 
-# WIN32 - graphical application/don't spawn a console (Windows only).
+# WIN32 - graphical application/don't spawn a console and use WinMain as entry point (Windows only).
 add_executable(LunaticVibesF WIN32 game.cpp LunaticVibesF.exe.manifest)
 target_link_libraries(LunaticVibesF PRIVATE gamelib PRIVATE tinyfiledialogs)
 set_target_properties(LunaticVibesF PROPERTIES CXX_STANDARD 23)

commit b5d1037040f45de1795e0bd049d8af60cfc819fc

style: format code base
Fixes: 0c3501275562 ("refactor(game): explicitly include SDL_main for WinMain")

src/game/game.cpp
@@ -43,7 +43,7 @@
 
 #ifdef RENDER_SDL2
 #include <SDL_main.h> // WinMain implementation
-#endif // RENDER_SDL2
+#endif                // RENDER_SDL2
 
 #include <boost/nowide/args.hpp>
 