thenist/lunaticvibesf

commit 5be551a71643da0dd4471546ae06c6ccd0a59172

feat(game): stub out LR2OOL number definitions
References https://github.com/chown2/lunaticvibesf/issues/223 "Add
LR2OOL Number Definitions".

src/game/skin/skin_lr2_number.cpp
@@ -62,6 +62,7 @@ int lunaticvibes::get_number(IndexNumber ind)
 {
     // Sources:
     // LR2
+    // LR2OOL - https://github.com/tenaibms/LR2OOL/blob/c5505559ed5cc86e1355fee3f6f9e967efaab08f/src/hooks/srcnumber.cpp
     // beatoraja
     switch (std::to_underlying(ind))
     {
@@ -83,10 +84,26 @@ int lunaticvibes::get_number(IndexNumber ind)
             return static_cast<int>(ruleset->getHealthAnimation().animate({}) * 100);
         break; // might still also be in State::get
 
+    case 295:           // TODO // LR2OOL: current random
+    case 296:           // TODO // LR2OOL: whole part of mean
+    case 297:           // TODO // LR2OOL: decimal part of mean
+    case 298:           // TODO // LR2OOL: whole part of stddev
+    case 299: return 0; // TODO // LR2OOL: decimal part of stddev
+
     case 301: // LR2HelperG
         if (auto chart = get_chart_for_display(gSelectContext, gChartContext); chart != nullptr)
             return getEffectiveChartTotal(*chart, convertGaugeType(State::get(IndexOption::PLAY_GAUGE_TYPE_1P)));
         return 0;
+
+    case 400: return State::get(IndexNumber::PLAY_1P_PERFECT) / State::get(IndexNumber::PLAY_1P_GREAT); // LR2OOL
+    case 401:                                                                                           // LR2OOL
+        return decimal_part(static_cast<double>(State::get(IndexNumber::PLAY_1P_PERFECT)) /
+                            State::get(IndexNumber::PLAY_1P_GREAT));
+    case 402: return State::get(IndexNumber::PLAY_1P_PERFECT) / State::get(IndexNumber::PLAY_1P_GOOD); // LR2OOL
+    case 403:                                                                                          // LR2OOL
+        return decimal_part(static_cast<double>(State::get(IndexNumber::PLAY_1P_GREAT)) /
+                            State::get(IndexNumber::PLAY_1P_GOOD));
+
     case 407: // beatoraja
         if (auto ruleset = std::dynamic_pointer_cast<RulesetBMS>(gPlayContext.ruleset[PLAYER_SLOT_PLAYER]); ruleset)
             return decimal_part(ruleset->getHealthAnimation().animate({}) * 100.);

commit 8ca120b839e47f5eb9fe01e04e5ca395812c6460

fix(game): avoid division by zero in new LR2OOL numbers
Fixes: 5be551a71643 ("feat(game): stub out LR2OOL number definitions")

src/game/skin/skin_lr2_number.cpp
@@ -58,6 +58,16 @@ static unsigned get_current_chart_length(SelectContextParams& select_ctx, ChartC
     return static_cast<int>((n - static_cast<int>(n)) * 100);
 }
 
+[[nodiscard]] static int ratio_whole(int x, int y)
+{
+    return y == 0 ? x : x / y;
+}
+
+[[nodiscard]] static int ratio_decimal(int x, int y)
+{
+    return y == 0 ? x : decimal_part(static_cast<double>(x) / y);
+}
+
 int lunaticvibes::get_number(IndexNumber ind)
 {
     // Sources:
@@ -95,14 +105,14 @@ int lunaticvibes::get_number(IndexNumber ind)
             return getEffectiveChartTotal(*chart, convertGaugeType(State::get(IndexOption::PLAY_GAUGE_TYPE_1P)));
         return 0;
 
-    case 400: return State::get(IndexNumber::PLAY_1P_PERFECT) / State::get(IndexNumber::PLAY_1P_GREAT); // LR2OOL
-    case 401:                                                                                           // LR2OOL
-        return decimal_part(static_cast<double>(State::get(IndexNumber::PLAY_1P_PERFECT)) /
-                            State::get(IndexNumber::PLAY_1P_GREAT));
-    case 402: return State::get(IndexNumber::PLAY_1P_PERFECT) / State::get(IndexNumber::PLAY_1P_GOOD); // LR2OOL
-    case 403:                                                                                          // LR2OOL
-        return decimal_part(static_cast<double>(State::get(IndexNumber::PLAY_1P_GREAT)) /
-                            State::get(IndexNumber::PLAY_1P_GOOD));
+    case 400: // LR2OOL
+        return ratio_whole(State::get(IndexNumber::PLAY_1P_PERFECT), State::get(IndexNumber::PLAY_1P_GREAT));
+    case 401: // LR2OOL
+        return ratio_decimal(State::get(IndexNumber::PLAY_1P_PERFECT), State::get(IndexNumber::PLAY_1P_GREAT));
+    case 402: // LR2OOL
+        return ratio_whole(State::get(IndexNumber::PLAY_1P_GREAT), State::get(IndexNumber::PLAY_1P_GOOD));
+    case 403: // LR2OOL
+        return ratio_decimal(State::get(IndexNumber::PLAY_1P_GREAT), State::get(IndexNumber::PLAY_1P_GOOD));
 
     case 407: // beatoraja
         if (auto ruleset = std::dynamic_pointer_cast<RulesetBMS>(gPlayContext.ruleset[PLAYER_SLOT_PLAYER]); ruleset)