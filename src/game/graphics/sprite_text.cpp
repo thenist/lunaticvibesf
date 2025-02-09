thenist/lunaticvibesf

commit e3a19704d5310bd5d206a5f55295fb1d8424edf4

fix(game): don't render text sprites with empty height
> sprite_text.cpp:79:35: runtime error: -nan is outside the range of
> representable values of type 'int'

src/game/graphics/sprite_text.cpp
@@ -34,9 +34,17 @@ void SpriteText::updateText()
     if (!_draw)
         return;
 
-    State::get(textInd, _textBuf);
-    updateTextTexture(_textBuf, _current.color);
-    updateTextRect();
+    if (textureRect.h == 0)
+    {
+        // Happens on RED BELT Result.
+        updateTextTexture("", _current.color); // Clear it.
+    }
+    else
+    {
+        State::get(textInd, _textBuf);
+        updateTextTexture(_textBuf, _current.color);
+        updateTextRect();
+    }
 }
 
 void SpriteText::updateTextTexture(std::string_view text, const Color& c)
@@ -74,9 +82,11 @@ void SpriteText::updateTextTexture(std::string_view text, const Color& c)
 void SpriteText::updateTextRect()
 {
     // fitting
-    Rect textRect = textureRect;
-    double sizeFactor = static_cast<double>(_current.rect.h) / textRect.h;
-    int text_w = static_cast<int>(std::round(textRect.w * sizeFactor));
+    if (textureRect.h == 0)
+        return;
+    const double sizeFactor = static_cast<double>(_current.rect.h) / textureRect.h;
+    LVF_DEBUG_ASSERT(!std::isnan(sizeFactor));
+    int text_w = static_cast<int>(std::round(textureRect.w * sizeFactor));
     switch (align)
     {
     case TEXT_ALIGN_LEFT: break;

commit ec90440cd39468ba758db1a26bef60ec3c3b0908

fix(game): do render text sprites
Fixes default key config having no rendered text.
The problem from that commit was fixed by zero check in updateTextRect.
Still, something funky is going on there, as we don't seem to reset
`_current.rect`.

Fixes: e3a19704d531 ("fix(game): don't render text sprites with empty height")

src/game/graphics/sprite_text.cpp
@@ -34,17 +34,9 @@ void SpriteText::updateText()
     if (!_draw)
         return;
 
-    if (textureRect.h == 0)
-    {
-        // Happens on RED BELT Result.
-        updateTextTexture("", _current.color); // Clear it.
-    }
-    else
-    {
-        State::get(textInd, _textBuf);
-        updateTextTexture(_textBuf, _current.color);
-        updateTextRect();
-    }
+    State::get(textInd, _textBuf);
+    updateTextTexture(_textBuf, _current.color);
+    updateTextRect();
 }
 
 void SpriteText::updateTextTexture(std::string_view text, const Color& c)