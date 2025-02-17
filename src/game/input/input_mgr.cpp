thenist/lunaticvibesf

commit 4756a41681c10a81fc91b61e73adb2073aa578a7

refactor(game): move mouse position getting to window layer
Maybe not the best idea.

src/game/input/input_mgr.cpp
@@ -5,6 +5,7 @@
 #include <config/config_mgr.h>
 #include <game/graphics/graphics.h>
 #include <shared_mutex>
+#include <tuple>
 
 #ifdef RENDER_SDL2
 #include <game/graphics/SDL2/input.h>
@@ -200,17 +201,7 @@ std::pair<std::bitset<Input::KEY_COUNT>, InputMgr::ScratchData> InputMgr::detect
 
 bool InputMgr::getMousePos(int& x, int& y)
 {
-#ifdef RENDER_SDL2
-    {
-        std::shared_lock l{sdl::state::g_input_mutex};
-        x = sdl::state::g_mouse_x;
-        y = sdl::state::g_mouse_y;
-    }
-    bool ret = true;
-#else
-#error "No mouse pos getting implementation"
-#endif
-    if (ret)
+    std::tie(x, y) = lunaticvibes::graphics::get_mouse_pos();
     {
         double canvasScaleX = graphics_get_canvas_scale_x();
         double canvasScaleY = graphics_get_canvas_scale_y();
@@ -219,7 +210,7 @@ bool InputMgr::getMousePos(int& x, int& y)
         if (canvasScaleY != 1.0)
             y = (int)std::floor(y / canvasScaleY);
     }
-    return ret;
+    return true;
 }
 
 void InputMgr::setDebounceTime(int ms)

src/game/graphics/SDL2/window_SDL2.cpp
@@ -706,3 +706,9 @@ void lunaticvibes::graphics::queue_screenshot(Path png)
     LOG_INFO << "[SDL2] Screenshot: " << png;
     screenshotPath = std::move(png);
 }
+
+std::pair<int, int> lunaticvibes::graphics::get_mouse_pos()
+{
+    std::shared_lock l{sdl::state::g_input_mutex};
+    return {sdl::state::g_mouse_x, sdl::state::g_mouse_y};
+}

src/game/graphics/graphics.h
@@ -82,4 +82,5 @@ void stopTextInput();
 namespace lunaticvibes::graphics
 {
 void queue_screenshot(Path png);
+std::pair<int, int> get_mouse_pos();
 } // namespace lunaticvibes::graphics

commit b2d8158c236e491bf946b3b29f37172210ceb4bf

refactor(game): remove unused includes from input_mgr
Fixes: 4756a41681c1 ("refactor(game): move mouse position getting to window layer")

src/game/input/input_mgr.cpp
@@ -4,12 +4,7 @@
 #include <common/types.h>
 #include <config/config_mgr.h>
 #include <game/graphics/graphics.h>
-#include <shared_mutex>
 #include <tuple>
-
-#ifdef RENDER_SDL2
-#include <game/graphics/SDL2/input.h>
-#endif
 
 InputMgr InputMgr::_inst;
 