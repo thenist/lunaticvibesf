thenist/lunaticvibesf

commit d73c0bd4f401476a539158391e9f30b4751982eb

fix(common): bms: consider mismatched random blocks for game mode detect
LN, mine, invisible note detection still won't work right for these
charts. Too much spaghetti. This needs better rewrite to handle them.

Fixes https://github.com/chown2/lunaticvibesf/issues/198 "Random lane
glitch".

src/common/chartformat/chartformat_bms.cpp
@@ -103,6 +103,7 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
 
     // implicit parameters
     bool hasDifficulty = false;
+    bool only_check_note_existence = false;
 
     for (StringContent lineBuf, lineBuf_; std::getline(bmsFile, lineBuf_);)
     {
@@ -242,10 +243,14 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
                         ifValue = std::stack<int>();
                     }
                 }
-                else if (ifValue.empty() || ifValue.top() != randomValue.top())
+                else if (ifValue.empty())
                 {
-                    // skip orphan blocks and mismatch IF value blocks
+                    // skip orphan blocks IF value blocks
                     continue;
+                }
+                else if (ifValue.top() != randomValue.top())
+                {
+                    only_check_note_existence = true;
                 }
             }
 
@@ -418,41 +423,44 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
 
                     if (x_ == 0) // 0x: basic info
                     {
-                        switch (_y)
+                        if (!only_check_note_existence)
                         {
-                        case 1: // 01: BGM
-                            seqToLane36(chBGM[bgmLayersCount[bar]][bar], value);
-                            ++bgmLayersCount[bar];
-                            break;
-                        case 2: // 02: Bar Length
-                            metres[bar] = toDouble(value);
-                            haveMetricMod = true;
-                            break;
-                        case 3: // 03: BPM change
-                            seqToLane16(chBPMChange[bar], value);
-                            haveBPMChange = true;
-                            break;
-                        case 4: // 04: BGA Base
-                            seqToLane36(chBGABase[bar], value);
-                            haveBGA = true;
-                            break;
-                        case 6: // 06: BGA Poor
-                            seqToLane36(chBGAPoor[bar], value);
-                            haveBGA = true;
-                            break;
-                        case 7: // 07: BGA Layer
-                            seqToLane36(chBGALayer[bar], value);
-                            haveBGA = true;
-                            break;
-                        case 8: // 08: ExBPM
-                            seqToLane36(chExBPMChange[bar], value);
-                            haveBPMChange = true;
-                            break;
-                        case 9: // 09: Stop
-                            seqToLane36(chStop[bar], value);
-                            haveStop = true;
-                            break;
-                        default: LOG_DEBUG << "[BMS] Unhandled x=0 (basic info) << y=" << _y; break;
+                            switch (_y)
+                            {
+                            case 1: // 01: BGM
+                                seqToLane36(chBGM[bgmLayersCount[bar]][bar], value);
+                                ++bgmLayersCount[bar];
+                                break;
+                            case 2: // 02: Bar Length
+                                metres[bar] = toDouble(value);
+                                haveMetricMod = true;
+                                break;
+                            case 3: // 03: BPM change
+                                seqToLane16(chBPMChange[bar], value);
+                                haveBPMChange = true;
+                                break;
+                            case 4: // 04: BGA Base
+                                seqToLane36(chBGABase[bar], value);
+                                haveBGA = true;
+                                break;
+                            case 6: // 06: BGA Poor
+                                seqToLane36(chBGAPoor[bar], value);
+                                haveBGA = true;
+                                break;
+                            case 7: // 07: BGA Layer
+                                seqToLane36(chBGALayer[bar], value);
+                                haveBGA = true;
+                                break;
+                            case 8: // 08: ExBPM
+                                seqToLane36(chExBPMChange[bar], value);
+                                haveBPMChange = true;
+                                break;
+                            case 9: // 09: Stop
+                                seqToLane36(chStop[bar], value);
+                                haveStop = true;
+                                break;
+                            default: LOG_DEBUG << "[BMS] Unhandled x=0 (basic info) << y=" << _y; break;
+                            }
                         }
                     }
                     else // not 0x
@@ -467,69 +475,72 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
                         unsigned chIdx = idx + (side == 1 ? 10 : 0);
                         if (side >= 0)
                         {
-                            switch (x_)
+                            if (!only_check_note_existence)
                             {
-                            case 1: // 1x: 1P visible
-                            case 2: // 2x: 2P visible
-                                seqToLane36(chNotesRegular[chIdx][bar], value);
-                                haveNote = true;
-                                if (side == 1)
-                                    haveAny_2 = true;
-                                break;
-                            case 3: // 3x: 1P invisible
-                            case 4: // 4x: 2P invisible
-                                seqToLane36(chNotesInvisible[chIdx][bar], value);
-                                haveInvisible = true;
-                                if (side == 1)
-                                    haveAny_2 = true;
-                                break;
-                            case 5: // 5x: 1P LN
-                            case 6: // 6x: 2P LN
-                                haveLNchannels = true;
-                                if (!lnobjSet.empty())
+                                switch (x_)
                                 {
-                                    // Note: there is so many possibilities of conflicting LN definition. Add all LN
-                                    // channel notes as regular notes
-                                    channel noteLane;
-                                    seqToLane36(noteLane, value, channel::NoteParseValue::LN);
-                                    unsigned scale =
-                                        chNotesRegular[chIdx][bar].relax(noteLane.resolution) / noteLane.resolution;
-                                    for (auto& note : noteLane.notes)
-                                    {
-                                        note.segment *= scale;
-                                        bool noteInserted = false;
-                                        channel& chTarget = chNotesRegular[chIdx][bar];
-                                        for (auto it = chTarget.notes.begin(); it != chTarget.notes.end(); ++it)
-                                        {
-                                            if (it->segment > note.segment ||
-                                                (it->segment == note.segment && it->value > note.value))
-                                            {
-                                                chTarget.notes.insert(it, note);
-                                                noteInserted = true;
-                                                break;
-                                            }
-                                        }
-                                        if (!noteInserted)
-                                        {
-                                            chTarget.notes.push_back(note);
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    // #LNTYPE 1
-                                    seqToLane36(chNotesLN[chIdx][bar], value, channel::NoteParseValue::LN);
-                                    haveLN = true;
+                                case 1: // 1x: 1P visible
+                                case 2: // 2x: 2P visible
+                                    seqToLane36(chNotesRegular[chIdx][bar], value);
+                                    haveNote = true;
                                     if (side == 1)
                                         haveAny_2 = true;
+                                    break;
+                                case 3: // 3x: 1P invisible
+                                case 4: // 4x: 2P invisible
+                                    seqToLane36(chNotesInvisible[chIdx][bar], value);
+                                    haveInvisible = true;
+                                    if (side == 1)
+                                        haveAny_2 = true;
+                                    break;
+                                case 5: // 5x: 1P LN
+                                case 6: // 6x: 2P LN
+                                    haveLNchannels = true;
+                                    if (!lnobjSet.empty())
+                                    {
+                                        // Note: there is so many possibilities of conflicting LN definition. Add all LN
+                                        // channel notes as regular notes
+                                        channel noteLane;
+                                        seqToLane36(noteLane, value, channel::NoteParseValue::LN);
+                                        unsigned scale =
+                                            chNotesRegular[chIdx][bar].relax(noteLane.resolution) / noteLane.resolution;
+                                        for (auto& note : noteLane.notes)
+                                        {
+                                            note.segment *= scale;
+                                            bool noteInserted = false;
+                                            channel& chTarget = chNotesRegular[chIdx][bar];
+                                            for (auto it = chTarget.notes.begin(); it != chTarget.notes.end(); ++it)
+                                            {
+                                                if (it->segment > note.segment ||
+                                                    (it->segment == note.segment && it->value > note.value))
+                                                {
+                                                    chTarget.notes.insert(it, note);
+                                                    noteInserted = true;
+                                                    break;
+                                                }
+                                            }
+                                            if (!noteInserted)
+                                            {
+                                                chTarget.notes.push_back(note);
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        // #LNTYPE 1
+                                        seqToLane36(chNotesLN[chIdx][bar], value, channel::NoteParseValue::LN);
+                                        haveLN = true;
+                                        if (side == 1)
+                                            haveAny_2 = true;
+                                    }
+                                    break;
+                                case 0xD: // Dx: 1P mine
+                                case 0xE: // Ex: 2P mine
+                                    seqToLane36(chMines[chIdx][bar], value);
+                                    haveMine = true;
+                                    break;
+                                default: LOG_VERBOSE << "[BMS] Unhandled x=" << x_ << "; y=" << _y; break;
                                 }
-                                break;
-                            case 0xD: // Dx: 1P mine
-                            case 0xE: // Ex: 2P mine
-                                seqToLane36(chMines[chIdx][bar], value);
-                                haveMine = true;
-                                break;
-                            default: LOG_VERBOSE << "[BMS] Unhandled x=" << x_ << "; y=" << _y; break;
                             }
 
                             switch (idx)

test/common/test_chartformat_bms.cpp
@@ -6,6 +6,7 @@
 #include <common/utils.h>
 
 #include <optional>
+#include <random>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -113,6 +114,16 @@ TEST(tBMS, MetaNoFile)
 
     EXPECT_FALSE(bms.checkHasReadme());
     EXPECT_TRUE(bms.getReadmeFiles().empty());
+}
+
+TEST(tBMS, RandomKeyCount)
+{
+    std::shared_ptr<ChartFormatBMS> bms = nullptr;
+    // Intentionally random seed. Who knows if default '0' would always trigger what was the #IF bug.
+    const auto seed = std::random_device{}();
+    ASSERT_NO_THROW(bms = std::make_shared<ChartFormatBMS>("bms/random_5k_7k.bme", seed));
+    ASSERT_EQ(bms->isLoaded(), true);
+    EXPECT_EQ(bms->gamemode, 7);
 }
 
 TEST(tBMS, RankInvalidParsedCorrectly)

test/test/bms/random_5k_7k.bme
@@ -1,0 +1,39 @@
+
+*---------------------- HEADER FIELD
+
+#TITLE title
+#BPM 150
+
+
+
+*---------------------- MAIN DATA FIELD
+
+
+
+#00111:01
+#00112:0001000000000000
+#00113:00010000
+#00114:0000000100000000
+#00115:0001
+#00116:01
+
+#RANDOM 2
+#IF 2
+#00118:0000000000010000
+#00119:00000001
+#ENDIF
+#ENDRANDOM
+
+#00211:0001000000000000
+#00212:0000010000000000
+#00213:0000000100000000
+#00214:0000000001000000
+#00215:0000000000010000
+#00216:0100000000000000
+
+#RANDOM 2
+#IF 2
+#00218:0000000000000100
+#00219:0000000000000001
+#ENDIF
+#ENDRANDOM

commit 3c581987237604617476f475356d0df11e1660a3

fix(common): reset only_check_note_existence after #ENDIF
Fixes https://github.com/chown2/lunaticvibesf/issues/219 "Some BMS chart
bugged".

Fixes: d73c0bd4f401 ("fix(common): bms: consider mismatched random blocks for game mode detect")

src/common/chartformat/chartformat_bms.cpp
@@ -127,7 +127,7 @@ int ChartFormatBMS::initWithText(std::stringstream& bmsFile, eFileEncoding encod
 
     // implicit parameters
     bool hasDifficulty = false;
-    bool only_check_note_existence = false;
+    bool only_check_note_existence = false; // TODO: check if it works when RANDOM is nested. It probably doesn't.
 
     for (StringContent lineBuf, lineBuf_; std::getline(bmsFile, lineBuf_);)
     {
@@ -222,6 +222,7 @@ int ChartFormatBMS::initWithText(std::stringstream& bmsFile, eFileEncoding encod
                         {
                             randomUsedValues.top().emplace(ifValue.top());
                             ifValue.pop();
+                            only_check_note_existence = false;
                         }
                         else
                         {

test/common/test_chartformat_bms.cpp
@@ -461,3 +461,12 @@ TEST(tBMS, ImplicitDifficulty)
     EXPECT_EQ(get_difficulty("#SUBTITLE testINSANEtest"), 4);
     EXPECT_EQ(get_difficulty("#TITLE test [InSaNe]"), 4);
 }
+
+TEST(tBMS, RandomInHeader)
+{
+    ChartFormatBMS bms("bms/random_in_header.bms");
+    ASSERT_EQ(bms.isLoaded(), true);
+
+    EXPECT_TRUE(bms.haveRandom);
+    EXPECT_EQ(bms.notes_total, 1);
+}

test/test/bms/random_in_header.bms
@@ -1,0 +1,20 @@
+// https://github.com/chown2/lunaticvibesf/issues/219
+// https://venue.bmssearch.net/letsbmsedit3/10
+
+*---------------------- HEADER FIELD
+
+#BMP02 misslayer.png
+
+*---------------------- EXPANSION FIELD
+#RANDOM 2
+#IF 1
+#BMP01 BGA dark.wmv
+#ENDIF
+#IF 2
+#BMP01 BGA light.wmv
+#ENDIF
+
+*---------------------- MAIN DATA FIELD
+
+#00111:01
+