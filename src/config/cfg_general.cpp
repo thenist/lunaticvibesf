thenist/lunaticvibesf

commit 961bc5f1c970b1789b8416698704a52fb87b0e88

refactor(game): make v-sync a boolean toggle

src/game/graphics/SDL2/window_SDL2.cpp
@@ -341,10 +341,10 @@ void lunaticvibes::window::save_new_window_size(int width, int height)
     ConfigMgr::General()->set(cfg::V_DISPLAY_RES_Y, height);
 }
 
-void lunaticvibes::window::graphics_change_vsync(lunaticvibes::GRAPHICS_VSYNC_MODE mode)
+void lunaticvibes::window::graphics_change_vsync(bool on)
 {
-    LOG_INFO << "[SDL2] Setting vsync mode to " << mode;
-    SDL_RenderSetVSync(gFrameRenderer, mode);
+    LOG_INFO << "[SDL2] Setting vsync mode to " << on;
+    SDL_RenderSetVSync(gFrameRenderer, on ? 1 : 0);
 }
 
 static int superSampleLevel = 1;

src/game/skin/skin_lr2_button_callbacks.cpp
@@ -1048,7 +1048,7 @@ static void vsync(int plus)
 
     SoundMgr::playSysSample(SoundChannelType::KEY_SYS, eSoundSample::SOUND_O_CHANGE);
 
-    lunaticvibes::window::graphics_change_vsync(static_cast<lunaticvibes::GRAPHICS_VSYNC_MODE>(val));
+    lunaticvibes::window::graphics_change_vsync(val % 1 == 1);
 }
 
 // 83

src/game/graphics/graphics.h
@@ -25,12 +25,6 @@ enum class GRAPHICS_WINDOW_MODE
     BORDERLESS = 2,
     FAKE_FULLSCREEN = 3,
 };
-// TODO: enum class
-enum GRAPHICS_VSYNC_MODE : int
-{
-    GRAPHICS_VSYNC_MODE_OFF = 0,
-    GRAPHICS_VSYNC_MODE_VSYNC = 1,
-};
 } // namespace lunaticvibes
 
 namespace lunaticvibes::window
@@ -53,7 +47,7 @@ void graphics_resize_window(int x, int y);
 // save passed windows size as the new one, without applying it (for that refer to graphics_resize_window)
 void save_new_window_size(int x, int y);
 
-void graphics_change_vsync(lunaticvibes::GRAPHICS_VSYNC_MODE mode);
+void graphics_change_vsync(bool);
 
 // scaling functions
 void graphics_set_supersample_level(int scale);

src/game/scene/scene_select.h
@@ -249,7 +249,7 @@ private:
     int old_video_mode, imgui_video_mode; // 0:windowed 1:fullscreen 2:borderless
     int imgui_video_ssLevel;
 
-    int imgui_video_vsync_index;
+    bool _imgui_enable_vsync;
 
     int imgui_video_maxFPS;
 

src/game/scene/scene_select_imgui.cpp
@@ -98,7 +98,7 @@ void SceneSelect::imguiInit()
     }
     old_video_display_resolution_index = imgui_video_display_resolution_index;
 
-    imgui_video_vsync_index = ConfigMgr::General()->get(cfg::V_VSYNC, 0);
+    _imgui_enable_vsync = ConfigMgr::General()->get(cfg::V_VSYNC, false);
 
     imgui_video_maxFPS = ConfigMgr::General()->get(cfg::V_MAXFPS, 240);
 
@@ -738,11 +738,7 @@ void SceneSelect::imguiPageOptionsVideo()
             imguiRefreshVideoDisplayResolutionList();
         }
 
-        ImGui::TextUnformatted(i18n::c(VIDEO_VSYNC));
-        ImGui::SameLine(infoRowWidth);
-        const char* imgui_vsync_mode_display[] = {i18n::c(OFF), i18n::c(ON)};
-        ImGui::Combo("##vsync", &imgui_video_vsync_index, imgui_vsync_mode_display,
-                     sizeof(imgui_vsync_mode_display) / sizeof(char*));
+        ImGui::Checkbox(i18n::c(VIDEO_VSYNC), &_imgui_enable_vsync);
 
         ImGui::TextUnformatted(i18n::c(VIDEO_MAXFPS));
         ImGui::SameLine(infoRowWidth);
@@ -1582,8 +1578,7 @@ bool SceneSelect::imguiApplyResolution()
         lunaticvibes::window::graphics_resize_window(windowW, windowH);
     }
     lunaticvibes::window::graphics_set_supersample_level(imgui_video_ssLevel);
-    lunaticvibes::window::graphics_change_vsync(
-        static_cast<lunaticvibes::GRAPHICS_VSYNC_MODE>(imgui_video_vsync_index));
+    lunaticvibes::window::graphics_change_vsync(_imgui_enable_vsync);
 
     if (imgui_video_mode != old_video_mode)
     {
@@ -1598,7 +1593,7 @@ bool SceneSelect::imguiApplyResolution()
         ConfigMgr::General()->set(cfg::V_WINMODE, windowMode);
     }
     ConfigMgr::General()->set(cfg::V_RES_SUPERSAMPLE, imgui_video_ssLevel);
-    ConfigMgr::General()->set(cfg::V_VSYNC, imgui_video_vsync_index);
+    ConfigMgr::General()->set(cfg::V_VSYNC, _imgui_enable_vsync);
 
     // windowed
     {
@@ -1618,18 +1613,8 @@ bool SceneSelect::imguiApplyResolution()
     }
     // vsync
     {
-        State::set(IndexOption::SYS_VSYNC, imgui_video_vsync_index == 0 ? 0 : 1);
-
-        static const std::map<int, std::string> smap = {
-            {0, "OFF"},
-            {1, "ON"},
-        };
-
-        auto&& s = imgui_video_vsync_index;
-        if (auto it = smap.find(s); it != smap.end())
-            State::set(IndexText::VSYNC, it->second);
-        else
-            State::set(IndexText::VSYNC, "OFF");
+        State::set(IndexOption::SYS_VSYNC, static_cast<int>(_imgui_enable_vsync));
+        State::set(IndexText::VSYNC, _imgui_enable_vsync ? "ON" : "OFF");
     }
 
     return true;

commit 04821ee42751fcd6a9104f2b55b7cc75a5b02cee

fix: always use booleans when referring to VSYNC
Looks like YAML-cpp or the way we use it doesn't implicitly convert
well.

Fixes https://github.com/chown2/lunaticvibesf/issues/230 "V-Sync not
working after next startup".

Fixes: 961bc5f1c970 ("refactor(game): make v-sync a boolean toggle")

src/config/cfg_general.cpp
@@ -17,7 +17,7 @@ void ConfigGeneral::setDefaults() noexcept
     set(V_DISPLAY_RES_Y, CANVAS_HEIGHT);
     set(V_WINMODE, V_WINMODE_WINDOWED);
     set(V_MAXFPS, 480);
-    set(V_VSYNC, 1);
+    set(V_VSYNC, true);
     set(E_PROFILE, PROFILE_DEFAULT);
     set(E_LR2PATH, ".");
     set(E_FOLDERS, std::vector<std::string>());

src/config/config_mgr.cpp
@@ -691,17 +691,7 @@ static void setText()
     }
 
     // vsync
-    {
-        static const std::map<int, string> smap = {{0, Option::s_vsync_mode[Option::VSYNC_OFF]},
-                                                   {1, Option::s_vsync_mode[Option::VSYNC_ON]},
-                                                   {2, Option::s_vsync_mode[Option::VSYNC_ADAPTIVE]}};
-
-        auto&& s = ConfigMgr::General()->get<int>(V_VSYNC, 0);
-        if (auto it = smap.find(s); it != smap.end())
-            State::set(IndexText::VSYNC, it->second);
-        else
-            State::set(IndexText::VSYNC, Option::s_vsync_mode[Option::VSYNC_OFF]);
-    }
+    State::set(IndexText::VSYNC, ConfigMgr::General()->get(V_VSYNC, false) ? "ON" : "OFF");
 
     // autoadjust
     {

src/game/graphics/SDL2/window_SDL2.cpp
@@ -114,8 +114,8 @@ int lunaticvibes::window::graphics_init()
         graphics_set_maxfps(maxFPS);
 
         unsigned renderer_flags = SDL_RENDERER_TARGETTEXTURE;
-        if (ConfigMgr::General()->get(cfg::V_VSYNC, 0) != 0)
-            renderer_flags += SDL_RENDERER_PRESENTVSYNC;
+        if (ConfigMgr::General()->get(cfg::V_VSYNC, false))
+            renderer_flags |= SDL_RENDERER_PRESENTVSYNC;
         gFrameRenderer = SDL_CreateRenderer(gFrameWindow, -1, renderer_flags);
         if (!gFrameRenderer)
         {

src/game/runtime/i18n.h
@@ -43,7 +43,7 @@ enum i18nTextIndex
     VIDEO_FULLSCREEN,
     VIDEO_BORDERLESS,
     VIDEO_VSYNC,
-    VIDEO_ADAPTIVE,
+    OLD_VIDEO_ADAPTIVE,
     VIDEO_MAXFPS,
     AUDIO_DEVICE,
     AUDIO_REFRESH_DEVICE_LIST,

src/game/runtime/index/option.h
@@ -499,18 +499,6 @@ static constexpr const char* s_windowed[] = {
     "WINDOW",
 };
 
-enum e_vsync_mode
-{
-    VSYNC_OFF,
-    VSYNC_ON,
-    VSYNC_ADAPTIVE
-};
-static constexpr const char* s_vsync_mode[] = {
-    "OFF",
-    "ON",
-    "ADAPTIVE",
-};
-
 enum e_course_type
 {
     COURSE_NONSTOP,

src/game/skin/skin_lr2_button_callbacks.cpp
@@ -1045,7 +1045,7 @@ static void vsync(int plus)
     const unsigned val = (State::get(IndexOption::SYS_VSYNC) + 2 + plus) % 2;
 
     State::set(IndexOption::SYS_VSYNC, val);
-    State::set(IndexText::VSYNC, Option::s_vsync_mode[val]);
+    State::set(IndexText::VSYNC, val ? "ON" : "OFF");
 
     SoundMgr::playSysSample(SoundChannelType::KEY_SYS, eSoundSample::SOUND_O_CHANGE);
 